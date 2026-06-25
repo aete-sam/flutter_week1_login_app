8753a9dfd643b16269be89060d84c15f009f953c
Aetesam Asfar Ullah <aetesamasfarullah18@gmail.com>
2026-06-26 00:28:19 +0500

    Login page UI changes

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -18,41 +19,110 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
+      backgroundColor: const Color(0xFFF4E1E7),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Form(
           key: _formkey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/illustrations/login-back.png',
+                    height: 300,
+                    width: 300,
+                    fit: BoxFit.contain,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-
-              TextFormField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    'Welcome Back!',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 48,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFFDCC2CA),
+                    ),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
+                const SizedBox(height: 40),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    labelStyle: GoogleFonts.montserrat(
+                      color: const Color(0xFF4A4458),
+                      fontWeight: FontWeight.w500,
+                    ),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF4A4458)),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF353A48),
+                        width: 2,
+                      ),
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.email,
+                      color: Color(0xFF4A4458),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
 
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const Home()),
-                  );
-                },
-                child: const Text('Login'),
-              ),
-            ],
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: GoogleFonts.montserrat(
+                      color: const Color(0xFF4A4458),
+                      fontWeight: FontWeight.w500,
+                    ),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF4A4458)),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFF353A48),
+                        width: 2,
+                      ),
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.lock,
+                      color: Color(0xFF4A4458),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                SizedBox(
+                  width: double.maxFinite,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF353A48),
+                      foregroundColor: const Color(0xFFFFFFFF),
+                      padding: const EdgeInsets.symmetric(vertical: 18),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const Home()),
+                      );
+                    },
+                    child: const Text('Login'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
